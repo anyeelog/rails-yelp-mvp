5a0f4e48579908fd13d9db259b234c77caff99d8
Angie Xu <anyixu98@gmail.com>
2023-02-22 12:32:40 +0100

    Created controllers with methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+# Rails.application.routes.draw do
+#   resources :restaurants do
+#     resources :reviews, only: [:new, :create]
+# 	# we put it inside because we don't need to have the restaurant id inside the url
+#   end
+#   resources :reviews, only: [:destroy]
+# 	# we put it outside because we need to have the restaurant id inside the url
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: CATEGORIES.sample
+  )
+  restaurant.save!
+end
+
+puts "Finished!"
